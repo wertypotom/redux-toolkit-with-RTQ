7b27fcfe34bcfbdc62d900f7417f49553b3df3b6
Andrey Povstyanko <werty.potom@gmail.com>
2023-01-05 03:42:06 +0600

    added RTK query GET POST DELETE PUT

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,16 +1,51 @@
 import React from 'react';
-import { postApi } from '../services/PostService';
+import { IPost } from '../models/IPost';
+import {
+  postApi,
+  useCreatePostMutation,
+  useDeletePostMutation,
+  useFetchAllPostsQuery,
+  useUpdatePostMutation,
+} from '../services/PostService';
 
 const PostContainer = () => {
-  const { data: posts } = postApi.useFetchAllPostsQuery(5);
+  const { data: posts, isLoading, isError } = useFetchAllPostsQuery(100);
+  const [createPost, {}] = useCreatePostMutation();
+  const [remove, {}] = useDeletePostMutation();
+  const [update, {}] = useUpdatePostMutation();
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (isError) {
+    return <h1>Erorr happened...</h1>;
+  }
+
+  const createPostHandle = async () => {
+    await createPost({ id: Date.now(), title: 'mememe', body: 'hoooray' });
+  };
+
+  const removePost = (e: React.MouseEvent<HTMLButtonElement>, post: IPost) => {
+    e.stopPropagation();
+    remove(post);
+  };
+
+  const updatePost = (e: React.MouseEvent<HTMLDivElement>, post: IPost) => {
+    e.stopPropagation();
+    update({ ...post, title: prompt() || '' });
+  };
 
   return (
     <div>
+      <button onClick={createPostHandle}>Create Post</button>
+      <hr />
+
       {posts?.map((post) => (
-        <div key={post.id}>
+        <div key={post.id} onClick={(e) => updatePost(e, post)}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
-          <button>Remove post</button>
+          <button onClick={(e) => removePost(e, post)}>Remove post</button>
         </div>
       ))}
     </div>
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -4,8 +4,9 @@ import { IPost } from '../models/IPost';
 export const postApi = createApi({
   reducerPath: 'postApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://jsonplaceholder.typicode.com',
+    baseUrl: 'http://localhost:3000',
   }),
+  tagTypes: ['post'],
   endpoints: (builder) => {
     return {
       fetchAllPosts: builder.query<IPost[], number>({
@@ -15,9 +16,42 @@ export const postApi = createApi({
             _limit: limit,
           },
         }),
+        providesTags: () => ['post'],
+      }),
+
+      createPost: builder.mutation<IPost, IPost>({
+        query: (post: IPost) => ({
+          url: '/posts',
+          method: 'POST',
+          body: post,
+        }),
+        invalidatesTags: ['post'],
+      }),
+
+      updatePost: builder.mutation<IPost, IPost>({
+        query: (post: IPost) => ({
+          url: `/posts/${post.id}`,
+          method: 'PUT',
+          body: post,
+        }),
+        invalidatesTags: ['post'],
+      }),
+
+      deletePost: builder.mutation<IPost, IPost>({
+        query: (post: IPost) => ({
+          url: `/posts/${post.id}`,
+          method: 'DELETE',
+          body: post,
+        }),
+        invalidatesTags: ['post'],
       }),
     };
   },
 });
 
-export const { useFetchAllPostsQuery } = postApi;
+export const {
+  useFetchAllPostsQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postApi;
